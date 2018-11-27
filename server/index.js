0e8bbecdc1df5c6e69c1ec327d6110815d326f1d
Cortney Thomas <thomas.cortney@outlook.com>
2018-11-26 19:34:48 -0500

    stopped just before "creating a custom error page"

--- server/index.js
+++ server/index.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const routes = require('./routes');
 
+app.set('view engine', 'pug');
+if (app.get('env') === 'development') {
+    app.locals.pretty = true;
+}
+
+app.set('views', path.join(__dirname, './views'));
 app.use(express.static('public'));
 app.get('/favicon.ico', (req, res, next) => {
     return res.sendStatus(204);
